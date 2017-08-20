cdf8ee932bea53e64a9caf89554350851d15e636
Matias Dieguez <matiasdieguez@hotmail.com>
2017-08-19 22:49:00 -0300

    Agregado de proyectos db,api y apps

---
+++ src/MisPersonas/MisPersonasApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MisPersonasApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ src/MisPersonas/MisPersonasApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MisPersonasApi.WebApiApplication" Language="C#" %>
